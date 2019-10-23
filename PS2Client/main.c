72e731c53f5ef98e19d260dc5485344daefe5de9
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2019-10-23 18:22:09 +0200

    Add PS2Client test

---
+++ PS2Client/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tamtypes.h>
+#include <kernel.h>
+#include <sifrpc.h>
+
+void loop(void) {
+   int times = 0;
+
+   while (1)
+   {
+      /* code */
+      if (times == 100000000) {
+         printf("Hello, world!\n");
+         times = 0;
+      } else {
+         times++;
+      }
+   }
+}
+
+int main(int argc, char *argv[])
+{
+   int i;
+   
+   for (i = 0; i < argc; ++i) {
+        printf("Argument #%d is %s\n", i, argv[i]);
+    }
+
+   loop();
+   
+   return 0;
+}
